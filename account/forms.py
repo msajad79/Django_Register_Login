fd78f3ea95e871db2467a74884f7d6d00704f67b
sajad <sajadph79@gmail.com>
2021-07-25 18:48:10 +0430

    init LoginForm

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from django.forms import widgets
 
 class RegisterForm(UserCreationForm):
     username = forms.CharField(max_length=30)
@@ -9,6 +10,10 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email','password1', 'password2')
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=30)
+    password = forms.CharField(max_length=256, widget=forms.PasswordInput())
 
     
 
